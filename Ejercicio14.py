30721a3c02a82a5f38579b7e82fd1b85e85ac66b
omartorrado <frikimoro@gmail.com>
2017-10-17 11:38:52 +0200

    ejercicio 14 completo

--- Ejercicio14.py
+++ Ejercicio14.py
@@ -6,12 +6,17 @@ def numPalabra(frase):
     repeticionesPalabra={}
     for x in frase:
         fraseSinAcentos+=unicodedata.normalize('NFD',x)[0]
-        print(x)
     fraseSinAcentos=fraseSinAcentos.lower()
     listaPalabras=fraseSinAcentos.split(" ")
-
-
-
     print(listaPalabras)
+    for x in listaPalabras:
+        vecesRepetida=0
+        if x not in repeticionesPalabra:
+            for y in listaPalabras:
+                if x==y:
+                    vecesRepetida+=1
+                    print(x,"=",y,"vecesRepetida:",vecesRepetida)
+                repeticionesPalabra[x] = vecesRepetida
+    print(repeticionesPalabra)
 
 numPalabra("En un lugar de la Mancha en la Mancha")
