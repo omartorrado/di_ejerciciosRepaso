f4f20ceaf16302585c34ac1050b53bf87141617d
omartorrado <frikimoro@gmail.com>
2017-10-17 12:41:54 +0200

    ejercicio 15 completo

--- Ejercicio15.py
+++ Ejercicio15.py
@@ -1,17 +1,17 @@
 import random
 
-def tiradaMediaDados(dados,caras):
+def tiradaMediaDados(dados,caras,numTirada):
     resultados=0
     for x in range(1,dados+1):
         numero=random.randrange(1,caras+1)
-        print("tirada:",numero)
+        print("tirada:"+str(numTirada),numero)
         resultados+=numero
-        print("Total "+str(resultados))
-        return resultados
+    print("Total "+str(resultados))
+    return resultados
 
 tiradas=[]
 for x in range (0,20):
-    tiradas.append(tiradaMediaDados(2,6))
+    tiradas.append(tiradaMediaDados(2,6,x+1))
 tiradas.sort()
-for x in range (1,7):
+for x in range (2,13):
     print("Ha salido",tiradas.count(x)," veces el",x)
